3bdb7e1d573a45a6ae933727c1219cbcdf6d2d1a
raoxikui <raoxikui@qq.com>
2026-08-06 18:03:36 +0800

    优化

--- src/OrganizationServiceProvider.php
+++ src/OrganizationServiceProvider.php
@@ -80,6 +80,51 @@ class OrganizationServiceProvider extends ServiceProvider
         return $this->menu = $menu;
     }
 
+    protected $auth = [
+        // 机构管理
+        ['name' => '新增', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.index', 'abbr' => 'create', 'custom_order' => 1],
+        ['name' => '删除', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.index', 'abbr' => 'delete', 'custom_order' => 2],
+        ['name' => '编辑', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.index', 'abbr' => 'update', 'custom_order' => 3],
+        ['name' => '查看', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.index', 'abbr' => 'showed', 'custom_order' => 4],
+        ['name' => '筛选', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.index', 'abbr' => 'search', 'custom_order' => 5],
+        ['name' => '部门', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.department', 'abbr' => 'department', 'custom_order' => 6],
+        ['name' => '职务', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.job', 'abbr' => 'job', 'custom_order' => 7],
+        ['name' => '授权', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.auth', 'abbr' => 'auth', 'custom_order' => 8],
+        // 员工管理
+        ['name' => '新增', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.worker', 'abbr' => 'create', 'custom_order' => 1],
+        ['name' => '删除', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.worker', 'abbr' => 'delete', 'custom_order' => 2],
+        ['name' => '编辑', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.worker', 'abbr' => 'update', 'custom_order' => 3],
+        ['name' => '查看', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.worker', 'abbr' => 'showed', 'custom_order' => 4],
+        ['name' => '筛选', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.worker', 'abbr' => 'search', 'custom_order' => 5],
+        ['name' => '一键导入', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.worker', 'abbr' => 'import', 'custom_order' => 6],
+        ['name' => '导出', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.worker', 'abbr' => 'export', 'custom_order' => 7],
+        // 学生管理
+        ['name' => '新增', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'create', 'custom_order' => 1],
+        ['name' => '删除', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'delete', 'custom_order' => 2],
+        ['name' => '编辑', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'update', 'custom_order' => 3],
+        ['name' => '查看', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'showed', 'custom_order' => 4],
+        ['name' => '筛选', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'search', 'custom_order' => 5],
+        ['name' => '班级管理', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'classes', 'custom_order' => 6],
+        ['name' => '一键导入', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'import', 'custom_order' => 7],
+        ['name' => '导出', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.student', 'abbr' => 'export', 'custom_order' => 8],
+        // 家长管理
+        ['name' => '新增', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.patriarch', 'abbr' => 'create', 'custom_order' => 1],
+        ['name' => '删除', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.patriarch', 'abbr' => 'delete', 'custom_order' => 2],
+        ['name' => '编辑', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.patriarch', 'abbr' => 'update', 'custom_order' => 3],
+        ['name' => '查看', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.patriarch', 'abbr' => 'showed', 'custom_order' => 4],
+        ['name' => '导出', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.patriarch', 'abbr' => 'export', 'custom_order' => 5],
+        // 基础设施
+        ['name' => '新增', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.facility', 'abbr' => 'create', 'custom_order' => 1],
+        ['name' => '删除', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.facility', 'abbr' => 'delete', 'custom_order' => 2],
+        ['name' => '编辑', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.facility', 'abbr' => 'update', 'custom_order' => 3],
+        ['name' => '查看', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.facility', 'abbr' => 'showed', 'custom_order' => 4],
+        // 设备管理
+        ['name' => '新增', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.device', 'abbr' => 'create', 'custom_order' => 1],
+        ['name' => '删除', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.device', 'abbr' => 'delete', 'custom_order' => 2],
+        ['name' => '编辑', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.device', 'abbr' => 'update', 'custom_order' => 3],
+        ['name' => '查看', 'namespace' => 'dagasmart.organization',  'code' => 'extension.enterprise.device', 'abbr' => 'showed', 'custom_order' => 4],
+    ];
+
     public function boot(): void
     {
         Queue::route(StudentImportJob::class, queue: 'student-import', connection: 'redis');
